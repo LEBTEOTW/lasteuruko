d4b4b307d383c2c706637827788086637f9fc010
Justin Halsall <justin@juice10.com>
2011-05-07 16:43:29 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lasteuruko::Application.config.secret_token = '7c34241bd555fc9ec1f86e5bb1755a10e48d3fc5169d68e84356ef1ab7d750e6ac991578e56837a914b20f8436e2a65d3eb2c43af766c7dbd344d24564c08d0f'
